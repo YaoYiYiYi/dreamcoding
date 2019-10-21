a6032269f6ae25b7ba3e36355366e46ac1e51f78
1140640248@qq.com <1140640248@qq.com>
2019-10-21 18:20:22 +0800

    wechat/shopping-2(modify)

--- wechat-APP/shopping/pages/cart/cart.js
+++ wechat-APP/shopping/pages/cart/cart.js
@@ -5,17 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-    carts:[],
-    hasList:false,
-    selectAllStatus:true,
-    totalPrice:''
+    carts: [],
+    hasList: false,
+    selectAllStatus: true,
+    totalPrice: ''
   },
-  getTotalPrice(){
+  selectList(e) {
+    console.log(e)
+    let index = e.currentTarget.dataset.index
+    let selected= `carts[${index}].selected`
+    this.setData({
+      [selected]: !this.data.carts[index].selected
+    })
+    this.getTotalPrice()
+    let carts = this.data.carts
+    for(let i=0;i<carts.length;i++){
+      if(!carts[i].selected){
+        this.setData({
+          selectAllStatus : false
+        })
+        return 
+      }
+      else {
+        this.setData({
+          selectAllStatus : true
+        })
+      }
+    }
+  },
+  selectAll(){
+    let selectAllStatus = this.data.selectAllStatus
+    selectAllStatus = !selectAllStatus
+    let carts =this.data.carts
+    for(let i= 0 ;i <carts.length;i++){
+      carts[i].selected = selectAllStatus
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  getTotalPrice() {
     let carts = this.data.carts
     let total = 0
-    for(let i=0 ;i<carts.length;i++){
-      if(carts[i].selected){
-        total+=carts[i].num*carts[i].price
+    for (let i = 0; i < carts.length; i++) {
+      if (carts[i].selected) {
+        total += carts[i].num * carts[i].price
       }
     }
     this.setData({
@@ -23,12 +59,52 @@ Page({
       //保留两位小数
     })
   },
+  addCount(e){
+    let index = e.currentTarget.dataset.index
+    let carts = this.data.carts
+    let num = carts[index].num
+    num++
+    carts[index].num = num
+    this.setData({
+      carts : carts
+    })
+    this.getTotalPrice()
+  },
+  minusCount(e){
+    let index = e.currentTarget.dataset.index
+    let carts = this.data.carts
+    let num = carts[index].num
+    num--
+    if(num<0){
+      num=0
+    }
+    carts[index].num = num
+    this.setData({
+      carts : carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e){
+    let index = e.currentTarget.dataset.index
+    let carts= this.data.carts
+    carts.splice(index,1)
+    this.setData({
+      carts:carts
+    })
+    if(!carts.length){
+      this.setData({
+        hasList :false
+      })
+    }
+    else{
+      this.getTotalPrice()
+    }
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -44,10 +120,10 @@ Page({
   onShow: function () {
     setTimeout(() => {
       this.setData({
-        hasList:true,
-        carts:[
+        hasList: true,
+        carts: [
           { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
-              { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
         ]
       })
       this.getTotalPrice()
