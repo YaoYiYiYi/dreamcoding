1d46a5f2bd5a5f8ac7fbec5ac0f66eac80396208
1140640248@qq.com <1140640248@qq.com>
2019-11-09 17:10:49 +0800

    cross

--- vue/vue-ele-app/src/components/goods/goods.vue
+++ vue/vue-ele-app/src/components/goods/goods.vue
@@ -19,10 +19,19 @@
       </div>
       <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
-          <li class="food-list" v-for="(item, index) in goods" :key="index" ref="foodlist">
+          <li
+          class="food-list"
+          v-for="(item, index) in goods"
+          :key="index"
+          ref="foodList"
+          >
             <h1 class="title">{{item.name}}</h1>
             <ul>
-              <li class="food-item border-1px" v-for="(food, index) in item.foods" :key="index">
+              <li
+              class="food-item border-1px"
+              v-for="(food, index) in item.foods"
+              :key="index"
+              >
                 <div class="icon">
                   <img :src="food.icon" alt="">
                 </div>
@@ -34,8 +43,12 @@
                     <span>好评率{{food.rating}}%</span>
                   </div>
                   <div class="price">
-                    <span class="now">￥{{food.price}}</span>
-                    <span class="old">￥{{food.pldPrice}}</span>
+                    <span class="now">¥{{food.price}}</span>
+                    <span class="old" v-if="food.oldPrice">¥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <!-- + -->
+                    <cartcontrol :food="food" @add="addFood"></cartcontrol>
                   </div>
                 </div>
               </li>
@@ -44,31 +57,43 @@
         </ul>
       </div>
     </div>
-    <v-shopcart></v-shopcart>
+    <!-- 购物车 -->
+    <shopcart
+      :selectFoods = "selectFoods"
+      :deliveryPrice = "seller.deliveryPrice"
+      :minPrice = "seller.minPrice"
+    ></shopcart>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll'
-import shopcart from '@/components/shopcart/shopcart.vue'
+import shopcart from '@/components/shopcart/shopcart'
+import cartcontrol from '@/components/cartcontrol/cartcontrol'
 export default {
   name: 'Goods',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
   data () {
     return {
-      goods: '',
-      classMap: ['decrease', 'discount', 'invoice', 'special', 'guarantee'],
+      goods: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
       listHeight: [],
       scrollY: 0
     }
   },
   components: {
-    'v-shopcart': shopcart
+    shopcart,
+    cartcontrol
   },
   created () {
     this.$http.get('http://localhost:8080/static/goods.json')
       .then((res) => {
+        console.log(res)
         if (res.data.errno === 0) {
-          console.log(res)
           this.goods = res.data.data
           this.$nextTick(() => {
             this._initScroll()
@@ -87,6 +112,19 @@ export default {
         }
       }
       return 0
+    },
+    selectFoods () {
+      let foods = []
+      for (let good of this.goods) {
+        if (good.foods) {
+          for (let food of good.foods) {
+            if (food.count) {
+              foods.push(food)
+            }
+          }
+        }
+      }
+      return foods
     }
   },
   methods: {
@@ -99,30 +137,34 @@ export default {
         probeType: 3
       })
       this.foodsScroll.on('scroll', pos => {
+        // console.log(pos)
         this.scrollY = Math.abs(Math.round(pos.y))
       })
     },
-    selectMenu (index) {
-      // this.currentIndex = index
-      let foodlist = this.$refs.foodlist
-      let el = foodlist[index]
+    selectMenu (idx) {
+      console.log(idx)
+      // this.currentIndex = idx
+      let foodList = this.$refs.foodList
+      let el = foodList[idx]
       this.foodsScroll.scrollToElement(el, 300)
     },
     _calculateHeight () {
-      let foodlist = this.$refs.foodlist
+      let foodList = this.$refs.foodList
       let height = 0
       this.listHeight.push(height)
-      for (let i = 0; i < foodlist.length; i++) {
-        let item = foodlist[i]
-        height = item.clientHeight + height
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
         this.listHeight.push(height)
       }
-      console.log(this.listHeight)
+    },
+    addFood () {
+
     }
   }
-
 }
 </script>
+
 <style lang="stylus" scoped>
 @import '../../common/stylus/mixin.styl'
 .goods
@@ -133,50 +175,50 @@ export default {
   width 100%
   overflow hidden
   .menu-wrapper
-        flex 0 0 80px
-        width 80px
-        background-color #f3f5f7
-        .menu-item
-            display table
-            width 56px
-            height 54px
-            padding 0 12px
-            line-height 14px
-            &.current
-                position relative
-                z-index 10px
-                margin-top -1px
-                background-color #fff
-                font-weight 700
-                .text
-                    border-none()
-            .icon
-                display inline-block
-                vertical-align top
-                width 12px
-                height 12px
-                margin-right 2px
-                background-size 12px 12px
-                background-repeat no-repeat
-                &.decrease
-                    bg-image('decrease_3')
-                &.discount
-                    bg-image('discount_3')
-                &.guarantee
-                    bg-image('guarantee_3')
-                &.invoice
-                    bg-image('invoice_3')
-                &.special
-                    bg-image('special_3')
-            .text
-                display table-cell
-                width 56px
-                vertical-align middle
-                border-1px(rgba(7, 17, 27, 0.1))
-                font-size 12px
+    flex 0 0 80px
+    width 80px
+    background #f3f5f7
+    .menu-item
+      display table
+      width 56px
+      height 54px
+      padding 0 12px
+      line-height 14px
+      &.current
+        position relative
+        z-index 10
+        margin-top -1px
+        background #fff
+        font-weight 700
+        .text
+          border-none()
+      .icon
+        display inline-block
+        vertical-align top
+        width 12px
+        height 12px
+        margin-right 2px
+        background-size 12px 12px
+        background-repeat no-repeat
+        &.decrease
+          bg-image('decrease_3')
+        &.discount
+          bg-image('discount_3')
+        &.guarantee
+          bg-image('guarantee_3')
+        &.invoice
+          bg-image('invoice_3')
+        &.special
+          bg-image('special_3')
+      .text
+        display table-cell
+        width 56px
+        vertical-align middle
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 12px
   .foods-wrapper
     flex 1
-    overflow scroll
+    // overflow scroll
     .title
       padding-left 14px
       height 26px
