e20874bc8698e5af4615f4a1136134946e0cad48
1140640248@qq.com <1140640248@qq.com>
2019-12-03 17:04:58 +0800

    mixin-vue

---
+++ vue/vue-souseCode/vue-mixin-drag-loading/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Dialog/>
+    <Loading/>
+  </div>
+</template>
+
+<script>
+import Dialog from './components/dialog'
+import Loading from './components/loading'
+export default {
+  name: 'App',
+  components: {
+    Loading,
+    Dialog
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ vue/vue-souseCode/vue-mixin-drag-loading/src/components/dialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span @mousedown="start" style="position: absolute; top: 0; left: 0">拖拽文本</span>
+    <el-button @click="open">open</el-button>
+    <el-dialog title="tips" :visible="visible">
+      <span @click="close">这是一段信息</span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// import
+import mixin from './visible'
+import dragAbleMix from './drag'
+export default {
+   mixins: [mixin, dragAbleMix]
+  // data () {
+  //   return {
+  //     visible: false
+  //   }
+  // },
+  // methods: {
+  //   open () {
+  //     this.visible = true
+  //   },
+  //   close () {  
+  //     this.visible = false
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
