7bfe6fee10c41dfa01238d6547b08d3ba42cca23
1140640248@qq.com <1140640248@qq.com>
2019-12-03 22:22:00 +0800

    登入注册界面的完成

--- vue/nodeBook/note_book/controllers/mySqlConfig.js
+++ vue/nodeBook/note_book/controllers/mySqlConfig.js
@@ -39,7 +39,14 @@ let getAllusers = function () {
   return allServices.query(_sql)
 }
 
+// 查找数据库是否存在用户的账号和密码
+let userLogin = function (username, userpwd) {
+  let _sql = `select * from users where username="${username}" and userpwd ="${userpwd}";`
+  return allServices.query(_sql)
+}
+
 module.exports = {
   getAllusers,
+  userLogin
 }
  
--- vue/nodeBook/note_book/routes/users.js
+++ vue/nodeBook/note_book/routes/users.js
@@ -17,4 +17,42 @@ router.get('/all', async(ctx, next) => {
   })
 })
 
+// 登入
+router.post('/userLogin', async(ctx, next) => {
+  var _username = ctx.request.body.username
+  var _userpwd = ctx.request.body.userpwd
+  console.log(_username,_userpwd)
+  await userServices.userLogin(_username, _userpwd)
+  .then((res) => {
+    let r = ''
+    console.log(res)
+    if(res.length) {
+      r = 'ok'
+      let result = {
+        id: res[0].id,
+        nickname: res[0].nickname,
+        username: res[0].username
+      }
+      ctx.body = {
+        code: '200',
+        data: result,
+        mess: '登入成功'
+      }
+    } else {
+      r = 'error';
+      ctx.body = {
+        code: '404',
+        data: r,
+        mess: '账号密码错误'
+      }
+    }
+  })
+  .catch((err) => {
+    ctx.body = {
+      code: '500',
+      data: err
+    }
+  })
+})
+
 module.exports = router
