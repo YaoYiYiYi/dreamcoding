758cbcc9bbe6341b26eeac9ea62174447224f078
1140640248@qq.com <1140640248@qq.com>
2019-12-10 23:36:41 +0800

    form表单的组件

---
+++ vue/vue-components/src/components/views/child2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child2">child2</div>
+</template>
+
+<script>
+import bus from '@/bus/bus'
+export default {
+  data () {
+    return {
+      mess2: ''
+    }
+  },
+  mounted () {
+    bus.$on('test', (text) => {
+      console.log(text)
+      this.mess2 = text
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vue/vue-components/src/router/index.js
+++ vue/vue-components/src/router/index.js
@@ -2,15 +2,27 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import A from '@/components/pages/A'
+import Parent from '@/components/views/parent'
+import Form from '@/components/form/show'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/a',
       name: 'A',
       component: A
+    },
+    {
+      path: '/parent',
+      name: 'Parent',
+      component: Parent
+    },
+    {
+      path: '/',
+      name: 'Form',
+      component: Form
     }
   ]
 })
