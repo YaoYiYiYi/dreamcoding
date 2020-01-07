3cece316ce6db1d565988b91e3069519a0e60078
1140640248@qq.com <1140640248@qq.com>
2020-01-07 10:11:17 +0800

    blog-demo

---
+++ vue/blog/blog-demo/src/components/BlogPost.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blogpost">
+    <h2>test</h2>
+  </div>
+
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+  .blogpost
+    h2
+      text-decoration underline
+</style>
