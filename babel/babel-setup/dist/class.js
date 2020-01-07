c43259feae708ef42feea610d61b8dd935838de7
1140640248@qq.com <1140640248@qq.com>
2020-01-07 14:31:44 +0800

    babel

---
+++ babel/babel-setup/dist/class.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var M = function M() {
+  (0, _classCallCheck2["default"])(this, M);
+};
+
+new M();
